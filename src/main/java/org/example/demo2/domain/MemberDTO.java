db7d1fbbf7545e8ac3acc9bc5bb2a1a14c679c9b
heejounglee <heejounglee301@gmail.com>
2026-01-09 13:24:55 +0900

    first commit

---
+++ src/main/java/org/example/demo2/domain/MemberDTO.java
@@ -0,0 +1,14 @@
+package org.example.demo2.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class MemberDTO {
+    private int memberId;
+    private String name;
+    private String address;
+}
